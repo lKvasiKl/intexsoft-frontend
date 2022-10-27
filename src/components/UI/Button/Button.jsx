95f9aa97ecde3bfcf640861f5317060e8966066c
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-10-27 23:04:57 +0300

    Create button component

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import style from './Button.module.css';
+
+console.log(style);
+
+const Button = ({children, onClick, outline, ...props}) => {
+    const {className, ...otherProps} = props;
+
+    return (
+        <button
+            onClick={onClick}
+            className={classNames(
+                className,
+                style.button
+            )}
+            {...otherProps}>
+            {children}
+        </button>
+    );
+};
+
+Button.propTyps = {
+    onClick: PropTypes.func
+};
+
+export default Button;
