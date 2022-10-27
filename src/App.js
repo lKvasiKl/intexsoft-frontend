09a9caf27d2bd491035392cff57c17829800980f
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-10-27 23:02:27 +0300

    Edit proj structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import Button from "./components/UI/Button";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const handleClick = () => alert('vsem ku');
+
+const App = () => {
+    return (
+        <div className="container">
+            <Button onClick={handleClick}>Click me</Button>
+        </div>
+    );
 }
 
 export default App;
