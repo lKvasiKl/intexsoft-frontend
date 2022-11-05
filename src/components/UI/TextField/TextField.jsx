85d81a2c74e065d4dc32bf9e257434469372f15d
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-05 18:13:26 +0300

    Edit TextField component

--- src/components/UI/TextField/TextField.jsx
+++ src/components/UI/TextField/TextField.jsx
@@ -1,34 +1,60 @@
-import React, {useMemo, useState} from 'react'
+import React, {useMemo, useRef, useState} from 'react'
 import classNames from 'classnames';
 import {BsEye, BsEyeSlash} from 'react-icons/bs';
 
-import styles from './TextField.module.css';
+import styles from './TextField.module.scss';
 
-const TextField = ({...props}) => {
-    const {className, type, ...otherProps} = props;
+const TextField = (props) => {
+    const {className, type, placeholder, ...otherProps} = props;
 
-    //const [inputFocused, setInputFocused] = useState(false);
+    const [inputFocused, setInputFocused] = useState(false);
     const [passwordVisible, setPasswordVisible] = useState(false);
 
+    const inputRef = useRef(null);
     const inputType = useMemo(() =>
             (type === "text" ? type : passwordVisible ? "text" : type),
         [passwordVisible, type]
     );
 
-    const handleEyeIconClick = () => {
+    const handleEyeIconClick = event => {
+        event.stopPropagation();
         setPasswordVisible(prevState => !prevState);
     };
 
+    const handleTextFieldClick = () => {
+        setInputFocused(true);
+        inputRef.current?.focus();
+    };
+
+    const handleInputFocus = event => {
+        if (props.onFocus) {
+            props.onFocus(event);
+        }
+        setInputFocused(true);
+    }
+
+    const handleInputBlur = event => {
+        if (props.onFocus) {
+            props.onFocus(event);
+        }
+        setInputFocused(false);
+    }
+
     return (
-        <div className={classNames(className)}>
-            <span className={classNames(styles.inputWrapper)}>
+        <div className={classNames(className, styles.textFieldWrapper)}>
+            <span className={classNames(styles.inputWrapper, {
+                [styles.focused]: inputFocused,
+                [styles.filled]: props.value || inputRef.current?.value
+            })}
+                  onClick={handleTextFieldClick}>
+                <label className={styles.label}>{placeholder}</label>
                 <div className={styles.inputContainer}>
                     <input
-                        className={classNames(
-                            className,
-                            styles.input
-                        )}
+                        ref={inputRef}
+                        className={styles.input}
                         type={inputType}
+                        onBlur={handleInputBlur}
+                        onFocus={handleInputFocus}
                         {...otherProps} />
                 </div>
                 {type === "password" && (
