33cb94bc0c87990e803e3d37a287096dc0a689d0
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-05 18:14:56 +0300

    Created LoginForm component

---
+++ src/feature/auth/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import TextField from "../../../../components/UI/TextField";
+import Checkbox from "../../../../components/UI/Chackbox";
+import Button from "../../../../components/UI/Button";
+
+import styles from './LoginForm.module.scss';
+
+const LoginForm = () => {
+    return (
+        <div className={styles.formContainer}>
+            <form className={styles.form}>
+                <TextField type="email" placeholder="Email"/>
+                <TextField type="password" placeholder="Password"/>
+                <div className={styles.utilsContainer}>
+                    <Checkbox
+                        label="Remember me"
+                    />
+                    {/*label заменить на <Link to={route}*/}
+                    <label className={styles.link}>
+                        Forgot password?
+                    </label>
+                </div>
+                <div className={styles.btnContainer}>
+                    <Button>
+                        Sign in
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
