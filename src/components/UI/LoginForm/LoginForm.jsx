6a5132b65f3b65487c020c79c8df785adc122230
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-24 16:36:54 +0300

    Edit login form component

---
+++ src/components/UI/LoginForm/LoginForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+//import {Link} from 'react-router-dom';
+import Input from "../Input/Input";
+import PasswordInput from "../PasswordInput/PasswordInput";
+import Checkbox from "../Chackbox/Checkbox";
+import Button from "../Button/Button";
+
+import styles from './LoginForm.module.scss';
+
+const LoginForm = ({buttonTheme, formTheme}) => {
+    const [form, setForm] = React.useState({});
+
+    const handleChangeForm = ({ name, value }) => {
+        setForm({ ...form, [name]: value });
+    };
+
+    return (
+        <div className={styles.formContainer}>
+            <form className={styles.form}>
+                <Input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <PasswordInput
+                    placeholder="Password"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <div className={styles.utilsContainer}>
+                    <Checkbox
+                        label="Remember me"
+                    />
+                    {/*label заменить на <Link to={route}*/}
+                    <label className={styles.link}>
+                        Forgot password?
+                    </label>
+                </div>
+                <div className={styles.btnContainer}>
+                    <Button
+                        theme={buttonTheme}
+                        //TODO: onClick={handleCheckUser}
+                        >
+                        Sign in
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
