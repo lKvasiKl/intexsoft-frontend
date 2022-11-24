5f4f3ae5e15697b63770c30ad02b1c9ceb816d93
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-24 16:38:12 +0300

    Add Register form component

---
+++ src/components/UI/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+//import {Link} from 'react-router-dom';
+import Input from "../Input/Input";
+import PasswordInput from "../PasswordInput/PasswordInput";
+import Button from "../Button/Button";
+
+import styles from './RegiterForm.module.scss';
+
+const RegisterForm = ({buttonTheme, formTheme}) => {
+    const [form, setForm] = React.useState({});
+
+    const handleChangeForm = ({ name, value }) => {
+        setForm({ ...form, [name]: value });
+    };
+
+    return (
+        <div className={styles.formContainer}>
+            <form className={styles.form}>
+                <Input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <Input
+                    type="text"
+                    name="firstName"
+                    placeholder="First name"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <Input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last name"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <PasswordInput
+                    placeholder="Password"
+                    theme={formTheme}
+                    onChange={handleChangeForm}
+                />
+                <div className={styles.btnContainer}>
+                    <Button
+                        //TODO: onClick saveUser
+                        theme={buttonTheme}
+                    >
+                        Sign up
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterForm;
