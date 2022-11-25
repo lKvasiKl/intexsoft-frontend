57bc580a67b91ddb5711fe5e63ebf19acf2d5d91
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-25 11:52:50 +0300

    Check auth

--- src/feature/auth/NotAuthRoute.jsx
+++ src/feature/auth/NotAuthRoute.jsx
@@ -1,6 +1,13 @@
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
+import {useAuth} from '../../hooks/useAuth';
 
 function NotAuthRoute({children}) {
+    const {isAuth} = useAuth;
+
+    if (isAuth) {
+        return <Navigate to="/home" />
+    }
+
     return children;
 }
 
